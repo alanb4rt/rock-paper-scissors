20ab7a77a3a4575a570bb3db4e765b4287c6f10f
alanb4rt <al4n.b4rt@gmail.com>
2024-12-11 13:10:03 +0100

    add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="max-w-screen-md mx-auto py-8">
+        <div className="flex items-center justify-between gap-8 p-4 border-4 border-white/50 rounded-2xl font-bold uppercase">
+          <div className="flex flex-col text-4xl leading-none">
+            <p>Rock</p>
+            <p>Paper</p>
+            <p>Scissors</p>
+          </div>
+          <div className="flex flex-col items-center py-4 px-12 rounded-lg bg-white text-[var(--text)]">
+            <p className="font-semibold tracking-widest">Score</p>
+            <p className="text-6xl">12</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
