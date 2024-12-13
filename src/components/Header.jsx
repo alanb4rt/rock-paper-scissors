1715dc176dd6124d67150813edbaa2ae4105e522
alanb4rt <al4n.b4rt@gmail.com>
2024-12-13 11:18:50 +0100

    refacto Header structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,15 @@
+import logo from "../assets/images/logo.svg";
+
 export default function Header() {
   return (
     <header>
       <div className="max-w-screen-md mx-auto py-8">
         <div className="flex items-center justify-between gap-8 p-4 border-4 border-white/50 rounded-2xl font-bold uppercase">
-          <div className="flex flex-col text-4xl leading-none">
-            <p>Rock</p>
-            <p>Paper</p>
-            <p>Scissors</p>
-          </div>
-          <div className="flex flex-col items-center py-4 px-12 rounded-lg bg-white text-[var(--text)]">
+          <img src={logo} alt="Logo" />
+          <section className="flex flex-col items-center py-4 px-12 rounded-lg bg-white text-[var(--text)]">
             <p className="font-semibold tracking-widest">Score</p>
             <p className="text-6xl">12</p>
-          </div>
+          </section>
         </div>
       </div>
     </header>
