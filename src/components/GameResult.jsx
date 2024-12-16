476927ca659341d13902cdafa76b5d76a8aea792
alanb4rt <al4n.b4rt@gmail.com>
2024-12-16 18:15:52 +0100

    add GameResult with static data

---
+++ src/components/GameResult.jsx
@@ -0,0 +1,18 @@
+import GameButton from "./GameButton";
+
+export default function GameResult() {
+  return (
+    <section>
+      <div className="relative flex items-stretch justify-center gap-16">
+        <article className="text-center">
+          <h2 className="text-2xl uppercase mb-6">You picked</h2>
+          <GameButton choice="paper" color="var(--paper-gradient)" />
+        </article>
+        <article className="text-center">
+          <h2 className="text-2xl uppercase mb-6">The house picked</h2>
+          <div className="animate-pulse size-32 mx-auto translate-y-4 bg-black/25 rounded-full"></div>
+        </article>
+      </div>
+    </section>
+  );
+}
