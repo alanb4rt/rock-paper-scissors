14e52240c7d115344c21f8f2f73a41a61b99d5ac
alanb4rt <al4n.b4rt@gmail.com>
2024-12-18 15:14:55 +0100

    refacto: add GameScore to Header

---
+++ src/components/GameScore.jsx
@@ -0,0 +1,8 @@
+export default function GameScore() {
+  return (
+    <section className="flex flex-col items-center py-4 px-12 rounded-lg bg-white text-[var(--text)]">
+      <p className="font-semibold tracking-widest">Score</p>
+      <p className="text-6xl">0</p>
+    </section>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logo from "../assets/images/logo.svg";
+import GameScore from "./GameScore";
 
 export default function Header() {
   return (
@@ -6,10 +7,7 @@ export default function Header() {
       <div className="max-w-screen-md mx-auto py-8">
         <div className="flex items-center justify-between gap-8 p-4 border-4 border-white/50 rounded-2xl font-bold uppercase">
           <img src={logo} alt="Logo" />
-          <section className="flex flex-col items-center py-4 px-12 rounded-lg bg-white text-[var(--text)]">
-            <p className="font-semibold tracking-widest">Score</p>
-            <p className="text-6xl">12</p>
-          </section>
+          <GameScore />
         </div>
       </div>
     </header>
