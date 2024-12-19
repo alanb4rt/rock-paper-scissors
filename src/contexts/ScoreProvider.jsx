f341628dc2cd58a56a29c7954bfd9e5d8a22559a
alanb4rt <al4n.b4rt@gmail.com>
2024-12-19 18:30:38 +0100

    feat(ScoreProvider): store the score in localStorage

--- src/contexts/ScoreProvider.jsx
+++ src/contexts/ScoreProvider.jsx
@@ -1,9 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const ScoreContext = createContext(0);
 
 export default function ScoreProvider({ children }) {
-  const [score, setScore] = useState(0);
+  const [score, setScore] = useState(() => {
+    const storedScore = localStorage.getItem("score");
+    return storedScore ? Number(storedScore) : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("score", score);
+  }, [score]);
 
   return (
     <ScoreContext.Provider value={{ score, setScore }}>
