b452a3b967aabfd667d0e407fba4859b05282df0
alanb4rt <al4n.b4rt@gmail.com>
2024-12-20 18:26:13 +0100

    style(GameButton): add shadows

--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -1,26 +1,17 @@
-const choices = {
-  rock: "var(--rock-gradient)",
-  scissors: "var(--scissors-gradient)",
-  paper: "var(--paper-gradient)",
-};
-
 export default function GameButton({ choice, handleChoice, isWinner }) {
-  const btnColor = choices[choice];
-
   return (
     <button
-      className={`relative mx-auto p-5 rounded-full ${
-        isWinner ? "winner" : ""
-      }`}
-      style={{ background: btnColor }}
+      className="mx-auto rounded-full shadow-xl"
       onClick={handleChoice ? () => handleChoice(choice) : undefined}
     >
-      <div className="grid place-content-center bg-white p-8 rounded-full">
-        <img
-          className="size-16 object-contain"
-          src={`./src/assets/images/icon-${choice}.svg`}
-          alt={`Icon ${choice}`}
-        />
+      <div className={`token token-${choice} ${isWinner ? "winner" : ""}`}>
+        <div className="grid place-content-center bg-white p-8 rounded-full shadow-[inset_0_8px_0_hsla(225_25_75/.5)]">
+          <img
+            className="size-16 object-contain"
+            src={`./src/assets/images/icon-${choice}.svg`}
+            alt={`${choice} icon`}
+          />
+        </div>
       </div>
     </button>
   );
