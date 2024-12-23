69fdb15f4b9385d6779e9518bf316899220894d6
alanb4rt <al4n.b4rt@gmail.com>
2024-12-23 13:14:37 +0100

    responsive(GameButton): resize based on the device

--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -5,9 +5,9 @@ export default function GameButton({ choice, handleChoice, isWinner }) {
       onClick={handleChoice ? () => handleChoice(choice) : undefined}
     >
       <div className={`token token-${choice} ${isWinner ? "winner" : ""}`}>
-        <div className="grid place-content-center bg-white p-8 rounded-full shadow-[inset_0_8px_0_hsla(225_25_75/.5)]">
+        <div className="grid place-content-center bg-white p-6 sm:p-8 rounded-full shadow-[inset_0_6px_0_hsla(225_25_75/.5)] sm:shadow-[inset_0_8px_0_hsla(225_25_75/.5)]">
           <img
-            className="size-16 object-contain"
+            className="size-12 sm:size-16 object-contain"
             src={`./src/assets/images/icon-${choice}.svg`}
             alt={`${choice} icon`}
           />
