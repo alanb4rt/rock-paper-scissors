34874515c45735d4a4a2ca5f8c7b4c7837972080
alanb4rt <al4n.b4rt@gmail.com>
2025-01-03 20:18:50 +0100

    refacto(GameResult): change layout to grid for better responsive

--- src/components/GameResult.jsx
+++ src/components/GameResult.jsx
@@ -35,30 +35,39 @@ export default function GameResult({ playerChoice, setPlayerChoice }) {
 
   return (
     <section>
-      <div className="relative flex items-stretch justify-center gap-16">
-        <article className="text-center">
-          <h2 className="text-2xl uppercase mb-6">You picked</h2>
-          <GameButton choice={playerChoice} isWinner={result === "You win"} />
-        </article>
-        {result !== "" && (
-          <article className="self-center text-center">
-            <h2 className="text-5xl mb-4 uppercase">{result}</h2>
-            <button
-              className="bg-white text-[var(--text)] hover:text-red-400 tracking-widest uppercase px-12 py-2 rounded-lg"
-              onClick={() => setPlayerChoice("")}
-            >
-              Play again
-            </button>
+      <div className="max-w-screen-lg mx-auto">
+        <div
+          className={`relative grid grid-cols-2 grid-rows-2 sm:grid-rows-1 gap-x-8 sm:gap-x-16 ${
+            result !== "" ? "sm:grid-cols-[1fr,auto,1fr]" : ""
+          }`}
+        >
+          <article className="justify-self-end text-center">
+            <h2 className="text-2xl uppercase mb-6">You picked</h2>
+            <GameButton choice={playerChoice} isWinner={result === "You win"} />
           </article>
-        )}
-        <article className="text-center">
-          <h2 className="text-2xl uppercase mb-6">The house picked</h2>
-          {houseChoice === "" ? (
-            <div className="animate-pulse size-32 mx-auto translate-y-4 bg-black/25 rounded-full"></div>
-          ) : (
-            <GameButton choice={houseChoice} isWinner={result === "You lose"} />
+          <article className="justify-self-start text-center sm:order-last">
+            <h2 className="text-2xl uppercase mb-6">The house picked</h2>
+            {houseChoice === "" ? (
+              <div className="animate-pulse size-32 mx-auto translate-y-4 bg-black/25 rounded-full"></div>
+            ) : (
+              <GameButton
+                choice={houseChoice}
+                isWinner={result === "You lose"}
+              />
+            )}
+          </article>
+          {result !== "" && (
+            <article className="col-span-2 sm:col-auto self-center text-center">
+              <h2 className="text-5xl mb-4 uppercase">{result}</h2>
+              <button
+                className="bg-white text-[var(--text)] hover:text-red-400 tracking-widest uppercase px-12 py-2 rounded-lg"
+                onClick={() => setPlayerChoice("")}
+              >
+                Play again
+              </button>
+            </article>
           )}
-        </article>
+        </div>
       </div>
     </section>
   );
