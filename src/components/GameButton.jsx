67ecdc344b19f0502dc509775767ca442e009944
alanb4rt <al4n.b4rt@gmail.com>
2025-01-10 16:27:53 +0100

    feat: add getImageURL for image during build

--- src/components/GameButton.jsx
+++ src/components/GameButton.jsx
@@ -1,4 +1,8 @@
+import { getImageURL } from "../utils/getImageURL";
+
 export default function GameButton({ choice, handleChoice, isWinner }) {
+  const imgURL = getImageURL(`icon-${choice}.svg`);
+
   return (
     <button
       className="mx-auto rounded-full shadow-xl last:col-span-2"
@@ -8,7 +12,7 @@ export default function GameButton({ choice, handleChoice, isWinner }) {
         <div className="grid place-content-center bg-white p-6 sm:p-8 rounded-full shadow-[inset_0_6px_0_hsla(225_25_75/.5)] sm:shadow-[inset_0_8px_0_hsla(225_25_75/.5)]">
           <img
             className="size-12 sm:size-16 object-contain"
-            src={`./src/assets/images/icon-${choice}.svg`}
+            src={imgURL}
             alt={`${choice} icon`}
           />
         </div>
